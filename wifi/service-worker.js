1dedba33989e8c289efedbfe9a09a45f051ddea8
Glitch (glitch-hello-website) <none>
2024-09-12 23:32:39 +0000

    🐝🐃 Checkpoint ./wifi/index.html:62919950/1715 ./wifi/service-worker.js:62919950/620

---
+++ wifi/service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'offline-cache';
+const OFFLINE_URLS = [
+    '/wifi',
+    'online.png',
+    'offline.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(OFFLINE_URLS);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
