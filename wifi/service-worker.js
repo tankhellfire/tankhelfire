6f89fd908f94e758ce8bcf7da2e7accfb5a39dd7
Glitch (glitch-hello-website) <none>
2024-09-13 08:54:19 +0000

    🌥🙇 Checkpoint ./wifi/service-worker.js:62919950/7 ./wifi/index.html:62919950/1061

--- wifi/service-worker.js
+++ wifi/service-worker.js
@@ -16,7 +16,7 @@ self.addEventListener('fetch', event => {
   console.log('fetch',event)
     event.respondWith(
         caches.match(event.request).then(response => {
-            return response || fetch(event.request);
+            return 'a'//response //|| fetch(event.request);
         })
     );
 });
