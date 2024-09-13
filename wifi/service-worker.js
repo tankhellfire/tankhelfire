8141973d3cafdf09ae7baf0da0bdc39ec2c6267d
Glitch (glitch-hello-website) <none>
2024-09-13 09:05:26 +0000

    💓⛪️ Checkpoint ./wifi/index.html:62919950/4460 ./wifi/service-worker.js:62919950/2277

--- wifi/service-worker.js
+++ wifi/service-worker.js
@@ -1,24 +1,25 @@
-const CACHE_NAME = 'offline-cache';
+const CACHE_NAME = "offline-cache";
 const OFFLINE_URLS = [
-    'https://tankhellfire.glitch.me/wifi',
-    // 'offline.png'
+  "/wifi",
+  // 'offline.png'
 ];
-self.addEventListener('install', event => {
-  console.log('install',event)
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(OFFLINE_URLS);
-        })
-    );
+self.addEventListener("install", (event) => {
+  console.log("install", event);
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(OFFLINE_URLS);
+    })
+  );
 });
 
-self.addEventListener('fetch', event => {
-  console.log('fetch',event)
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return 'a'//response //|| fetch(event.request);
-        })
-    );
+self.addEventListener("fetch", (event) => {
+  console.log("fetch", event);
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return "a"; //response //|| fetch(event.request);
+    })
+  );
 });
 
-console.log(self)
+console.log(self);
