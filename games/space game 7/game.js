594e6f28695f3925e1b39d0f749ec5ee3bfc1e68
Glitch (tankhellfire) <none>
2024-10-20 10:35:05 +0000

    🚏🛩 Checkpoint ./games/space game 7/game.js:62919950/592

--- games/space game 7/game.js
+++ games/space game 7/game.js
@@ -2,6 +2,8 @@ main.insertElement(document.createElement('display'), 1).id = "display";
 var a=main.insertElement(document.createElement('div'), 0)
 a.id = "text";
 a.style.top ='-50vh'
+a.style.left ='-50vw'
+a.style.width='100vw'
 a.style.position = "absolute";
 
 let pointer={x:0,y:0}
@@ -11,25 +13,27 @@ function update() {
   let upf = (performance.now() - lastFrameTime) / 1000;
   lastFrameTime = performance.now();
 
-  if(1){//mouse.pc.new
+  if(mouse.pc.new){
     pointer.x=mouse.pc.x
     pointer.y=mouse.pc.y
   }else{
-    let b=0
-    pointer.x=0
-    pointer.y=0
-    for (a of mouse.touch) {
-      if(!a){continue}
-      pointer.x+=a.x
-      pointer.y+=a.y
-      b++
+    if(mouse.touch.length!=0){
+      let b=0
+      pointer.x=0
+      pointer.y=0
+      for (a of mouse.touch) {
+        if(!a){continue}
+        pointer.x+=a.x
+        pointer.y+=a.y
+        b++
+      }
+      pointer.x/=b
+      pointer.y/=b
     }
-    pointer.x/=b
-    pointer.y/=b
   }
 
   game.physics(upf)
-  $('text').innerHTML=`${mouse.pc.mx}<br>${mouse.pc.my}<br><br>${pointer.x}<br>${pointer.y}`
+  $('text').innerHTML=`${pointer.x}<br>${pointer.y}<br><br>${JSON.stringify(mouse.touch)}`
   
   $("mouse").gotoxy({x:pointer.x, y:pointer.y});
 
