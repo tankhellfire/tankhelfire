88bb5b4351efbe49780f2731d2901902362c5bea
Glitch (tankhellfire) <none>
2024-11-01 12:20:04 +0000

    ⏰⛺️ Checkpoint ./timer/index.html:62919950/107 ./timer/game.js:62919950/4029

--- timer/game.js
+++ timer/game.js
@@ -15,19 +15,18 @@ new db("timer", "12h").then(async (e) => {
 let last = 0;
 async function update() {
   let now = new Date();
-  text.innerText = now;
 
   if (time < now) {
     showNotification("/daily");
     do {
-      clog('advacing time by 12h')
+      clog("advacing time by 12h");
       time.setHours(time.getHours() + 12);
     } while (time < now);
     data.set("time", time);
   }
   while (now < new Date(time).setHours(time.getHours() - 12)) {
-    clog('rewinding time by 12h')
-    time.setHours(time.getHours() - 12)
+    clog("rewinding time by 12h");
+    time.setHours(time.getHours() - 12);
   }
 
   let timetill = time - now;
@@ -40,27 +39,37 @@ async function update() {
     ":" +
     String(timetill % 1000).padStart(3, "0");
 
-  hours.innerText = String(time.getHours()).padStart(2, '0')
-  minutes.innerText=String(time.getMinutes()).padStart(2, '0')
-  seconds.innerText=String(time.getSeconds()).padStart(2, '0')
+  hours.innerText = String(time.getHours()).padStart(2, "0");
+  minutes.innerText = String(time.getMinutes()).padStart(2, "0");
+  seconds.innerText = String(time.getSeconds()).padStart(2, "0");
 
   last = now;
   requestAnimationFrame(update);
 }
 
-let i=0
-for (let ele of document.querySelectorAll('.time-segment')) {
-  let diff=(new Array(3)).fill(0)
-  diff[i]=1
-  const upButton = ele.querySelector('button:first-child');
-  const downButton = ele.querySelector('button:last-child');
-  const timeSpan = ele.querySelector('span');
-  clog(ele)
-  upButton.addEventListener('click', () => {
-      time.setHours(time.getHours()+diff[0],time.getMinutes()+diff[1],time.getSeconds()+diff[2])
-  })
-  downButton.addEventListener('click', () => {
-      time.setHours(time.getHours()-diff[0],time.getMinutes()-diff[1],time.getSeconds()-diff[2])
-  })
-  i++
+let i = 0;
+for (let ele of document.querySelectorAll(".time-segment")) {
+  let diff = new Array(3).fill(0);
+  diff[i] = 1;
+  const upButton = ele.querySelector("button:first-child");
+  const downButton = ele.querySelector("button:last-child");
+  const timeSpan = ele.querySelector("span");
+  clog(ele);
+  upButton.addEventListener("click", () => {
+    time.setHours(
+      time.getHours() + diff[0],
+      time.getMinutes() + diff[1],
+      time.getSeconds() + diff[2]
+    );
+    data.set("time", time);
+  });
+  downButton.addEventListener("click", () => {
+    time.setHours(
+      time.getHours() - diff[0],
+      time.getMinutes() - diff[1],
+      time.getSeconds() - diff[2]
+    );
+    data.set("time", time);
+  });
+  i++;
 }
