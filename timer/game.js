8385ba96a7f4241689a3b93a780859ed1095a797
Glitch (tankhellfire) <none>
2024-11-01 11:25:55 +0000

    🍁👽 Checkpoint ./timer/game.js:62919950/2271

--- timer/game.js
+++ timer/game.js
@@ -1,44 +1,47 @@
-let time
+let time;
 
 new db("timer", "12h").then(async (e) => {
   data = e;
   if ((await data.get("time")) == undefined) {
     var now = new Date();
-    data.set("time",new Date());
+    data.set("time", new Date());
   }
 
-  
-  time = await data.get('time');
+  time = await data.get("time");
 
   update();
 });
 //await navigator.storage.estimate()
-let last=0
+let last = 0;
 async function update() {
   let now = new Date();
   text.innerText = now;
 
-  if(time<now){
-    showNotification('/daily')
-    do{
-      time.setHours(time.getHours() + 12)
-    }while (time<now)
-    data.set("time",time);
+  if (time < now) {
+    showNotification("/daily");
+    do {
+      clog('advacing time by 12h')
+      time.setHours(time.getHours() + 12);
+    } while (time < now);
+    data.set("time", time);
   }
-  while(now<(new date(time)).setHours(time.getHours()-12)){
-    
+  while (now < new Date(time).setHours(time.getHours() - 12)) {
+    clog('rewinding time by 12h')
+    time.setHours(time.getHours() - 12)
   }
 
-  
-  let timetill=time-now
-  timer.innerText=
-    String(Math.floor(timetill / 3600000)%60).padStart(2, '0') + ':' +
-    String(Math.floor(timetill / 60000)%60).padStart(2, '0') + ':' +
-    String(Math.floor(timetill / 1000)%60).padStart(2, '0') + ':' +
-    String(timetill%1000).padStart(3, '0');
-  
-  alarmTime.innerText=time
+  let timetill = time - now;
+  timer.innerText =
+    String(Math.floor(timetill / 3600000) % 60).padStart(2, "0") +
+    ":" +
+    String(Math.floor(timetill / 60000) % 60).padStart(2, "0") +
+    ":" +
+    String(Math.floor(timetill / 1000) % 60).padStart(2, "0") +
+    ":" +
+    String(timetill % 1000).padStart(3, "0");
 
-  last=now
+  alarmTime.innerText = `${String(time.getHours()).padStart(2, '0')}:${String(time.getMinutes()).padStart(2, '0')}:${String(time.getSeconds()).padStart(2, '0')}:${String(time.getMilliseconds()).padStart(3, '0')}`;
+
+  last = now;
   requestAnimationFrame(update);
 }
