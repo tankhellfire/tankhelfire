63524a459c624e009710080f108a4160064427a0
Glitch (tankhellfire) <none>
2024-11-24 02:16:04 +0000

    🦄🐦 Checkpoint ./lib/export.js:62919950/227 ./space game 7/main.js:62919950/519 ./space game 7/game.js:62919950/17

--- space game 7/game.js
+++ space game 7/game.js
@@ -10,7 +10,7 @@ a.goto({
   left: -0.5,
 });
 
-let game = startGame();
+let game = new spaceGame7.startGame;
 buffer=''
 function update() {
   let dt = performance.now() / 1000 - time;//delta time
