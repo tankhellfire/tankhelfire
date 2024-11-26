4efd3644be7a4ec957b411efe073fd5d47fdfb2e
Glitch (tankhellfire) <none>
2024-11-26 11:29:12 +0000

    🚵🏎 Checkpoint ./space game 7/utill.js:62919950/9605

--- space game 7/utill.js
+++ space game 7/utill.js
@@ -6,50 +6,45 @@ Node.prototype.insertElement = function (element, index) {
 };
 
 Node.prototype.goto = function (pos) {
-  if (pos == undefined) {
-    return "?";
-  }
-  // clog(`${pos.x * 100}%`)
-  if (pos.x != undefined) {
-    if (typeof pos.x != "number") {
-      console.err("a x position was givin", pos.x, "but wasn't a number");
+  if (pos?.x != undefined) {
+    if (typeof pos?.x != "number") {
+      console.err("a x position was givin", pos?.x, "but wasn't a number");
     }
-    this.style.left = `${pos.x * 100}%`;
+    this.style.left = `${pos?.x * 100}%`;
   }
-  if (pos.y != undefined) {
-    if (typeof pos.y != "number") {
-      console.err("a y position was givin", pos.y, "but wasn't a number");
+  if (pos?.y != undefined) {
+    if (typeof pos?.y != "number") {
+      console.err("a y position was givin", pos?.y, "but wasn't a number");
     }
-    this.style.top = `${pos.y * 100}%`;
+    this.style.top = `${pos?.y * 100}%`;
   }
-  if (pos.left != undefined) {
-    if (typeof pos.left != "number") {
-      console.err("a left position was givin", pos.left, "but wasn't a number");
+  if (pos?.left != undefined) {
+    if (typeof pos?.left != "number") {
+      console.err("a left position was givin", pos?.left, "but wasn't a number");
     }
-    this.style.left = `${pos.left * 100}vw`;
+    this.style.left = `${pos?.left * 100}vw`;
   }
-  if (pos.top != undefined) {
-    if (typeof pos.top != "number") {
-      console.err("a top position was givin", pos.top, "but wasn't a number");
+  if (pos?.top != undefined) {
+    if (typeof pos?.top != "number") {
+      console.err("a top position was givin", pos?.top, "but wasn't a number");
     }
-    this.style.top = `${pos.top * 100}vh`;
+    this.style.top = `${pos?.top * 100}vh`;
   }
-  if (pos.center != undefined) {
-    if (typeof pos.center != "object") {
+  if (pos?.center != undefined) {
+    if (typeof pos?.center != "object") {
       console.err(
         "a center was givin",
-        pos.center,
+        pos?.center,
         "but wasn't a object (['x','y'])"
       );
     }
-    this.style.transform = `translate(${pos.center[0]}, ${pos.center[1]})`;
+    this.style.transform = `translate(${pos?.center[0]}, ${pos?.center[1]})`;
   }
-  let rot = Number(pos.rot ?? pos.rotation ?? pos.dir);
+  let rot = Number(pos?.rot ?? pos?.rotation ?? pos?.dir);
   if (rot != undefined) {
     if (typeof rot != "number") {
       console.err("a rotation was givin", rot, "but wasn't a number");
     }
-    // clog(pos.rot??pos.rotation??pos.dir)
     this.style.transform = `translate(-50%, -50%) rotate(${rot}turn) translate(50%, 50%)`;
   }
 
