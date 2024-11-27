bf6a7c7be62f45136e2ef1d800dedbdb18c3729a
Glitch (tankhellfire) <none>
2024-11-27 10:32:20 +0000

    🚎🍡 Checkpoint ./space game 7/main.js:62919950/83

--- space game 7/main.js
+++ space game 7/main.js
@@ -1,7 +1,9 @@
 ((n,f) => {if("object"==typeof exports){module.exports=exports=f()}else{if("function"==typeof define&&define.amd){define([], f)}else{this[n] = f()}}})(
 'spaceGame7',() => {
   class startGame{
-    
+    constructor({objCount=0}){
+      obj=new array(objCount)
+    };
     
     time=0;
     
