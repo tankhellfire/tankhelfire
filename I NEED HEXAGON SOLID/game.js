9aa9ea196f0ed0f4a75094b8ea71297a63b308ac
Glitch (tankhellfire) <none>
2024-11-30 05:40:21 +0000

    🌏🎍 Checkpoint ./I NEED HEXAGON SOLID/game.js:62919950/4085 ./I NEED HEXAGON SOLID/index.html:62919950/1194 ./I NEED HEXAGON SOLID/utill.js:62919950/5272

---
+++ I NEED HEXAGON SOLID/game.js
@@ -0,0 +1,174 @@
+let camera = {
+  x: .5,
+  y: .5,
+  z: 0,
+
+  pitch: 0,
+  yaw: 0,
+  roll: 0,
+  rotationSpeed: 1,
+  rotationMatrix: [],
+  scale: 2 / Math.tan(60 * 0.01745329)
+}
+main.insertElement(document.createElement("div"), 0).id = 'hud'
+hud.goto({
+  left: -1,
+  up: 1,
+})
+
+objs = [{
+  element: main.insertElement(document.createElement("div"), 0),
+  pos: [0, 0, 1]
+}, {
+  element: main.insertElement(document.createElement("div"), 0),
+  pos: [0, 1, 1]
+}, {
+  element: main.insertElement(document.createElement("div"), 0),
+  pos: [1, 0, 1]
+}, {
+  element: main.insertElement(document.createElement("div"), 0),
+  pos: [1, 1, 1]
+}, {
+  element: main.insertElement(document.createElement("div"), 0),
+  pos: [0, 0, 2]
+}, {
+  element: main.insertElement(document.createElement("div"), 0),
+  pos: [0, 1, 2]
+}, {
+  element: main.insertElement(document.createElement("div"), 0),
+  pos: [1, 0, 2]
+}, {
+  element: main.insertElement(document.createElement("div"), 0),
+  pos: [1, 1, 2]
+}, ]
+
+function update() {
+  let dt = performance.now() / 1000 - time;
+  //delta time
+  time = performance.now() / 1000;
+
+  camera.yaw += (pointer[pointer.primary]?.mx ?? 0) * camera.rotationSpeed
+  camera.pitch += (pointer[pointer.primary]?.my ?? 0) * camera.rotationSpeed
+
+  camera.roll = ((key.KeyE?.down ?? 0) - (key.KeyQ?.down ?? 0)) * .125 * (2 * Math.PI)
+
+  Movement({
+    KeyW: {
+      x: Math.sin(camera.yaw),
+      z: Math.cos(camera.yaw)
+    },
+    KeyA: {
+      x: -Math.cos(camera.yaw),
+      z: Math.sin(camera.yaw)
+    },
+    KeyS: {
+      x: -Math.sin(camera.yaw),
+      z: -Math.cos(camera.yaw)
+    },
+    KeyD: {
+      x: Math.cos(camera.yaw),
+      z: -Math.sin(camera.yaw)
+    },
+    Space:{
+      y:1
+    },
+    ShiftLeft:{
+      y:-1
+    }
+  }, .1)
+
+  camera.rotationMatrix = eulerToRotationMatrix(camera.pitch, camera.yaw, camera.roll)
+
+  for (let {element: ele, pos} of objs) {
+    let relitivePos = [pos[0] - camera.x, pos[1] - camera.y, pos[2] - camera.z]
+    let screenPos = vectBiasRotation(relitivePos, camera.rotationMatrix)
+
+    if (0 < screenPos[2]) {
+      ele.goto({
+        x: screenPos[0] / screenPos[2] * camera.scale,
+        y: screenPos[1] / screenPos[2] * camera.scale,
+      });
+      ele.innerText = JSON.stringify(pos)
+    } else {
+      ele.goto({
+        hidden: 1
+      })
+    }
+  }
+  hud.innerText = JSON.stringify(camera,null,2)
+
+  for (var a in key) {
+    key[a].new = 0;
+    key[a].fall = 0;
+    key[a].raise = 0;
+  }
+
+  for (var a in pointer)
+    (Object.assign(pointer[a], {
+      new: 0,
+      raise: 0,
+      fall: 0,
+      mx: 0,
+      my: 0,
+      mxPx: 0,
+      myPx: 0
+    }))
+  key.new = []
+  requestAnimationFrame(update);
+}
+update();
+// setInterval(update, 200)
+
+function Movement(moveKeys, speed) {
+  for (let a in moveKeys) {
+    if (key[a]?.down) {
+      camera.x += (moveKeys[a].x ?? 0) * speed
+      camera.y += (moveKeys[a]?.y ?? 0) * speed
+      camera.z += (moveKeys[a]?.z ?? 0) * speed
+    }
+  }
+}
+
+function vectBiasRotation(vect, bias) {
+  let out = []
+  for (let row in vect) {
+    out[row] = 0
+    for (let column in vect) {
+      out[row] += vect[column] * bias[row][column]
+    }
+  }
+  return out
+}
+
+function biasBiasRotation(bias1, bias2) {
+  let out = []
+  for (let row of bias2) {
+    out.push(vectBiasRotation(row, invertBais(bias1)))
+  }
+  return out
+}
+
+function invertBais(bais) {
+  let out = [[], [], []]
+  for (let row in bais) {
+    for (let column in bais) {
+      out[column][row] = bais[row][column]
+    }
+  }
+  return out
+}
+
+function eulerToRotationMatrix(pitch, yaw, roll) {
+  const cx = Math.cos(pitch)
+  const sx = Math.sin(pitch)
+  const cy = Math.cos(yaw)
+  const sy = Math.sin(yaw)
+  const cz = Math.cos(roll)
+  const sz = Math.sin(roll);
+
+  const pitchMatrix = [[1, 0, 0], [0, cx, -sx], [0, sx, cx]];
+  const rollMatrix = [[cz, -sz, 0], [sz, cz, 0], [0, 0, 1]];
+  const yawMatrix = [[cy, 0, -sy], [0, 1, 0], [sy, 0, cy]];
+
+  return biasBiasRotation(biasBiasRotation(yawMatrix, pitchMatrix), rollMatrix)
+}
---
+++ I NEED HEXAGON SOLID/utill.js
@@ -0,0 +1,250 @@
+Node.prototype.insertElement = function (element, index) {
+  return this.insertBefore(
+    element, //document.createElement(child_element_tagName)
+    this.children[index]
+  );
+};
+
+Node.prototype.goto = function (pos) {
+  if (pos.x != undefined) {
+    if (typeof pos?.x != "number") {
+      console.err("a x position was givin", pos?.x, "but wasn't a number");
+    }
+    this.style.left = `${pos?.x * 50}%`;
+  }
+  if (pos.y != undefined) {
+    if (typeof pos?.y != "number") {
+      console.err("a y position was givin", pos?.y, "but wasn't a number");
+    }
+    this.style.top = `${(0-pos?.y) * 50}%`;
+  }
+  if (pos.left != undefined) {
+    if (typeof pos?.left != "number") {
+      console.err("a left position was givin", pos?.left, "but wasn't a number");
+    }
+    this.style.left = `${pos?.left * 50}vw`;
+  }
+  if (pos.up != undefined) {
+    if (typeof pos?.up != "number") {
+      console.err("a top position was givin", pos?.up, "but wasn't a number");
+    }
+    this.style.top = `${(0-pos?.up) * 50}vh`;
+  }
+  if (pos.center != undefined) {
+    if (typeof pos?.center != "object") {
+      console.err(
+        "a center was givin",
+        pos?.center,
+        "but wasn't a object (['x','y'])"
+      );
+    }
+    this.style.transform = `translate(${pos?.center[0]}, ${pos?.center[1]})`;
+  }
+  let rot = Number(pos?.rot ?? pos?.rotation ?? pos?.dir);
+  if (rot != undefined) {
+    if (typeof rot != "number") {
+      console.err("a rotation was givin", rot, "but wasn't a number");
+    }
+    this.style.transform = `translate(-50%, -50%) rotate(${rot}turn) translate(50%, 50%)`;
+  }
+
+  if(pos?.hidden==1){
+    this.style.display = 'none';  
+  }else{
+    this.style.display = 'block';
+  }
+  this.style.position = "absolute";
+};
+
+const $ = (id) => document.getElementById(id);
+
+const clog = console.log;
+const keys = Object.keys;
+
+document.addEventListener(
+  "touchmove",
+  function (e) {
+    e.preventDefault();
+  },
+  {
+    passive: false,
+  }
+);
+
+document.addEventListener("click", (event) => {
+  document.body.requestPointerLock();
+  // document.documentElement.requestFullscreen();
+});
+
+// addEventListener(
+//   'beforeunload',
+//   function(e){
+//     console.log('a')
+//     e.stopPropagation();e.preventDefault();return false;
+//   },
+//   true
+// );
+
+let vmax;
+function setVmax() {
+  vmax = Math.max(window.innerWidth, window.innerHeight);
+}
+setVmax();
+window.addEventListener("resize", setVmax);
+
+let time = 0;
+
+let key={
+  new:[],
+  set:(e)=>{
+    
+    if(!key[e.code]){
+      key[e.code]={timeStamp:{key:e.timeStamp}}
+    }
+    
+    const ek=key[e.code]
+    const down=e.type==="keydown"
+    
+    key.new.push([e.code,e.key])
+    
+    Object.assign(ek,{
+      key:e.key,
+      new:1,
+      down,
+      up:!down,
+      fall:down&(key[e.code].up??1),
+      raise:!down&!(key[e.code].up??1),
+    })
+
+    for(let a of [
+      'new',
+      'fall',
+      'raise'
+    ]){
+      if(ek[a]){
+        ek.timeStamp[a]=e.timeStamp
+      }
+    }
+  }
+}
+document.addEventListener("keydown", (event) => {
+  // event.preventDefault();
+  key.set(event)
+});
+document.addEventListener("keyup", (event) => {
+  //event.preventDefault();
+  key.set(event)
+});
+
+function mouseUpdate() {
+  mouse.pc.new = 1;
+  mouse.pc.time = event.timeStamp;
+}
+
+let pointer={}
+function pointerHandler(event,sets={}) {
+  if(!pointer[event.pointerId]){
+    pointer[event.pointerId]={
+      time:null,
+      new:1,
+
+      pointerType:null,
+      
+      up:null,
+      down:null,
+      upTime:null,
+      downTime:null,
+
+      fall:null,
+      raise:null,
+
+      x:null,
+      y:null,
+      mx:0,
+      my:0,
+      
+      xPx:null,
+      yPx:null,
+      mxPx:0,
+      myPx:0,
+
+      startTarget:null,
+      target:null,
+      targets:null,
+    }
+  }
+  if(event.isPrimary){
+    pointer.primary=event.pointerId
+  }
+  const currentElement = document.elementFromPoint(
+    event.x,
+    event.y
+  );
+  const currentElements = document.elementsFromPoint(
+    event.x,
+    event.y
+  );
+  // clog(event)
+  Object.assign(pointer[event.pointerId],{
+    time:event.timeStamp,
+    new:1,
+    pointerType:event.pointerType,
+    
+    mx:2*(event.movementX/ vmax)+pointer[event.pointerId].mx??0,
+    my:-2*(event.movementY/ vmax)+pointer[event.pointerId].my??0,
+    x:((event.x - window.innerWidth / 2) / vmax) * 2,
+    y:-((event.y - window.innerHeight / 2) / vmax) * 2,
+    
+    mxPx:event.movementX,
+    myPx:-event.movementY,
+    xPx:event.x,
+    yPx:-event.y,
+    
+    startTarget:event.target,
+    target:currentElement,
+    targets:currentElements,
+  })
+  
+  Object.assign(pointer[event.pointerId],sets)
+}
+
+document.addEventListener("pointerdown",(event)=>{
+  pointerHandler(event,{
+    downTime:event.timeStamp,
+    down:1,
+    fall:1,
+    
+    up:0,
+  })
+})
+document.addEventListener("pointerup",(event)=>{
+  pointerHandler(event,{
+    upTime:event.timeStamp,
+    up:1,
+    raise:1,
+    
+    down:0,
+  })
+})
+document.addEventListener("pointermove",(event)=>{
+  pointerHandler(event)
+})
+
+document.addEventListener("pointerleave",(event)=>{
+  clog('deling',event.pointerId)
+  if(pointer.primary==event.pointerId){
+    delete pointer.primary
+  }
+  delete pointer[event.pointerId]
+})
+
+
+Math.clamp = (x, min, max) => {
+  if (x < min) {
+    return min;
+  }
+  if (max < x) {
+    return max;
+  }
+  return x;
+};
