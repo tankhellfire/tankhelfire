ff1d2c063b0a797db1fc0912e756b6239937dddd
Glitch (tankhellfire) <none>
2024-12-20 13:43:30 +0000

    🍢🌹 Checkpoint ./logic/index.html:62919950/25 ./lib/utill.js:62919950/4 ./logic/index.js:62919950/1019

--- logic/index.js
+++ logic/index.js
@@ -1,5 +1,13 @@
-(async()=>{
-  main.insertElement(document.createElement("img"), 0).id = "ico";
-  ico.src="assets/mouse.svg"
-  ico.title="ico"
-})()
+Object.assign(utill, {
+  pointerLock: 1,
+  fullscreen: 1,
+  keyPreventDefault: 1,
+});
+
+main.insertElement(document.createElement("img"), 0).id = "ico";
+ico.src = "assets/mouse.svg";
+ico.title = "ico"
+
+
+(async () => {
+})();
