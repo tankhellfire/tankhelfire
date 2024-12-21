0e829298b24f8a786750174bd4c5e9021e968055
Glitch (tankhellfire) <none>
2024-12-21 06:27:34 +0000

    🏠🏭 Checkpoint ./cache.js:62919950/1180

--- cache.js
+++ cache.js
@@ -6,17 +6,21 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
       console.log(`fetching "${event.request.url}"`)
-      let a=fetch(event.request)
+      let a=fetch(event.request).then(res=>{caches.open(CACHE_NAME).then(cache=>{
+          cache.put(event.request.url,res.clone())
+          console.log(`updated "${event.request.url}"`)
+        
+        })})
+      
+      // if(event.request.url.startsWith('https://tankhellfire.glitch.me')){
+      //   console.log('1')
+      // }
+      
       if(cachedResponse){
         console.log(`returning "${event.request.url}" from cache`)
         return cachedResponse
       }
-      if(event.request.url.startsWith('https://tankhellfire.glitch.me')){
-        a.then(res=>caches.open(CACHE_NAME).then(cache=>{
-          cache.put(event.request.url,res)
-          console.log(`updated "${event.request.url}"`)
-        }))
-      }
+      console.log('2')
       return a;
     })
   );
