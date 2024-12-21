337f37ee396738a17a6e0f71be3636758dd2f8e4
Glitch (tankhellfire) <none>
2024-12-21 06:38:42 +0000

    🐡⛷ Checkpoint ./cache.js:62919950/1074

--- cache.js
+++ cache.js
@@ -5,22 +5,24 @@ const CACHE_NAME = 'tankh-cache';
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
-      console.log(`fetching "${event.request.url}"`)
-      let a=fetch(event.request).then(res=>{caches.open(CACHE_NAME).then(cache=>{
-          cache.put(event.request.url,res.clone())
-          console.log(`updated "${event.request.url}"`)
+      let a=fetch(event.request).then(res=>{
         
-        })})
+        if(event.request.url.startsWith('https://tankhellfire.glitch.me')){
+          caches.open(CACHE_NAME).then(cache=>{
+            cache.put(event.request.url,res.clone())
+            // console.log(`updated "${event.request.url}"`)
+          })
+        }
+        
+        return res.clone()
+      })
       
-      // if(event.request.url.startsWith('https://tankhellfire.glitch.me')){
-      //   console.log('1')
-      // }
       
       if(cachedResponse){
-        console.log(`returning "${event.request.url}" from cache`)
+        // console.log(`returning "${event.request.url}" from cache`)
         return cachedResponse
       }
-      console.log('2')
+      // console.log(`returning "${event.request.url}" from fetch`)
       return a;
     })
   );
@@ -34,8 +36,6 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('activate', (event) => {
   console.log('Activating Service Worker...');
-  console.log(clients)
-
   event.waitUntil(clients.claim()); // Take control of all open pages
 });
 
