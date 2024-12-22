3419baa1d5c9a9148207de7f1b8cc6cc682d7261
Glitch (tankhellfire) <none>
2024-12-22 03:26:24 +0000

    👌🌽 Checkpoint ./logic/index.js:62919950/1026 ./logic/index.html:62919950/99

--- logic/index.js
+++ logic/index.js
@@ -16,11 +16,57 @@ b.src = "assets/mouse.svg";
 b.title = "ico";
 
 main.insertElement(document.createElement("div"), 0).id = "line";
-line.style.width='100vmax'
 line.classList.add('line')
 
 
+main.insertElement(document.createElement("div"), 0).id = "text";
+text.goto({})
+
+function drawLine(x1, y1, x2, y2) {
+  const line = document.getElementById('line');
+  
+  const dx = x2 - x1;
+  const dy = y1 - y2;
+  const length = Math.sqrt(dx * dx + dy * dy);
+  const angle = Math.atan2(dy, dx)
+  
+  // Style the line
+  line.style.width = `${length}vmax`;
+  line.style.transform = `translate(${x1}vmax, ${-y1}vmax) rotate(${angle}rad)`;
+}
 
 
-// (async () => {
-// })();
+
+
+async function update() {
+  //drawLine(0,0,pointer,10)
+  text.innerText=JSON.stringify(pointer)
+  
+  
+  
+  
+  
+  
+  
+  
+  for (var a in key) {
+    key[a].first = 0;
+    key[a].fall = 0;
+    key[a].raise = 0;
+  }
+  
+  for(var a in pointer)(
+    Object.assign(pointer[a],{
+      new:0,
+      raise:0,
+      fall:0,
+      mx:0,
+      my:0,
+      mxPx:0,
+      myPx:0
+    })
+  )
+  key.new=[]
+  requestAnimationFrame(update);
+}
+update();
