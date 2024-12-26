f9aff41a133af5918fec12f413f228bae27b805f
Glitch (tankhellfire) <none>
2024-12-26 07:45:37 +0000

    🎐🛀 Checkpoint ./cache/index.js:62919950/966 ./cache/index.html:62919950/417

--- cache/index.js
+++ cache/index.js
@@ -1,13 +1,30 @@
 let out={}
+
+function pass(inp,depth=0) {
+  if (typeof inp==='undefined') {return}
+  if (typeof inp==='function') {return 'func'}
+  if (typeof inp==='object') {
+    if(depth<0){
+      if(inp==null){return}
+      if(Object.keys(inp).length==0){return 'OOR'}
+      return inp
+    }
+    let ret={}
+    for (let a in inp){
+      ret[a]=pass(inp[a],depth-1)
+    }
+    if(Object.keys(ret).length==0){return {}}
+    return ret
+  }
+  return inp
+}
+
 (async()=>{
   for(let a of await caches.keys()){
-    clog(a)
     out[a]={}
     for(let b of await (await caches.open('tankh-cache')).matchAll()){
-      out[a][b.url]={
-        status:b.status
-      }
+      out[a][b.url]=pass(b,5)
     }
-    
   }
+  json.textContent  = JSON.stringify(out, null, 2)
 })()
