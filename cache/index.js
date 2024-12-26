55cd7ca7d06fd0ab9a7d0c0bd37c98bb95b6841a
Glitch (tankhellfire) <none>
2024-12-26 07:33:36 +0000

    👋🌸 Checkpoint ./cache/index.js:62919950/481 ./cache/index.html:62919950/3381

--- cache/index.js
+++ cache/index.js
@@ -1,6 +1,13 @@
+let out={}
 (async()=>{
   for(let a of await caches.keys()){
     clog(a)
-    cawait(await(caches.open('tankh-cache')).matchAll())
+    out[a]={}
+    for(let b of await (await caches.open('tankh-cache')).matchAll()){
+      out[a][b.url]={
+        status:b.status
+      }
+    }
+    
   }
 })()
