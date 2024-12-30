a4289f16974b2e0ca5a6fd73a5221d288981d86a
Glitch (tankhellfire) <none>
2024-12-30 05:59:19 +0000

    🍄🎨 Checkpoint ./cache.js:62919950/548 ./index.html:62919950/45

--- cache.js
+++ cache.js
@@ -6,21 +6,19 @@ const neverCache=[]
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
-      
-      const url=new URL(event.request.url,event.request.origin)
-      console.log(event.request)
+      const url=new URL(event.request.url)
       let a=fetch(event.request).then(res=>{
         
         
         if(url.hostname=='tankhellfire.glitch.me'){
           if(neverCache.includes(url.pathname)){
-            console.log(`//neverCache "${url.herf}"`)
+            console.log(`//never cache "${url.href}"`)
             return res.clone()
           }
           
           caches.open(CACHE_NAME).then(cache=>{
-            cache.put(url.herf,res.clone())
-            console.log(`//updated "${url.herf}"`)
+            cache.put(url.href,res.clone())
+            console.log(`//updated cache"${url.href}"`)
           })
         }
         
@@ -29,28 +27,24 @@ self.addEventListener('fetch', (event) => {
       
       
       if(cachedResponse){
+        console.log(`//returned cache "${url.href}"`)
         return cachedResponse
       }
+      console.log(`//returned fetch "${url.href}"`)
       return a;
     })
   );
 });
 
-// self.addEventListener('message', event => {
-//   if (event.data && event.data.type === 'FROM_PAGE') {
-//     console.log(event);
-//   }
-// });
-
 
 
 self.addEventListener('install', (event) => {
-  console.log('Installing Service Worker...');
+  console.log('//installing');
   self.skipWaiting(); // Activate immediately
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('Activating Service Worker...');
+  console.log('//activating');
   event.waitUntil(clients.claim()); // Take control of all open pages
 });
 
