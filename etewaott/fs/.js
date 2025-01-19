7d05762d4944f8b887174803de77428ece813411
Glitch (tankhellfire) <none>
2025-01-19 04:37:10 +0000

    🎭🌷 Checkpoint ./etewaott/fs/.js:62919950/3230

--- etewaott/fs/.js
+++ etewaott/fs/.js
@@ -13,7 +13,7 @@ class syncFs {
     this.text.innerText = "click to set location"
 
     this.button.onclick = async () => {
-      this.dir = await window.showDirectoryPicker()
+      this.dir = await window.showDirectoryPicker({id:`/etewaott/fs/index.html ${id}`})
       this.text.innerText = this.dir.name
       // alert("start")
       await this.makeOverVeiw()
@@ -106,120 +106,29 @@ class syncFs {
     return {val:c,file:this.overVeiw[c]}
   }
   setFromPath(pathArr){
-    let c=this.directoryOverVeiw
+    getFromPath()
+    let c=this.dir
     
-    for (let path of pathArr) {
-      if((c=c[path])===undefined){
-        return undefined
-      }
+    let i=0
+    for (i=0;i<pathArr.length-1;i++) {
+      c=c.getDirectoryHandle(pathArr[i],{make:1})
     }
     return {val:c,file:this.overVeiw[c]}
   }
-// for(let fromFile of fs[0].overVeiw){
-//   let toFile=fs[1].getFromPath(fromFile.path)?.file
-//   if(fromFile?.info?.size===toFile?.info?.size&&fromFile?.info?.hash?.sha256===toFile?.info?.hash?.sha256){
-//       console.log('same')
-//       continue
-//   }
-//   console.log('diff',fromFile,toFile)
-// }
-
-  dir;
-
-}
-
-fs = [new syncFs(0), new syncFs(1)]
-async function pullDir(handle, threads=1) {
-  let files = []
-
-  async function getAllPaths(handle, path=[]) {
-    let val = handle.values()
-    while (1) {
-      let {value, done} = await val.next()
-      if (done) {
-        break
-      }
-      if (value.kind === "directory") {
-        await getAllPaths(value, [...path, value.name]);
-        continue
-      }
-      if (value.kind === "file") {
-        files.push({
-          path: [...path, value.name],
-          handle: value
-        })
-        continue
-      }
-      console.warn(`unknown kind:"${value.kind}", @"${[...path, value.name].join('/')}" in`, value)
-    }
-  }
-
-  await getAllPaths(handle)
-  let i = 0
-  let wait = []
-
-  async function threadPassFile(thread) {
-    while (1) {
-      let jobNum = i++
-      info.textContent=jobNum
-      console.log(thread, 'new job', jobNum)
-      let job = files[jobNum]
-      if (job === undefined) {
-        break
-      }
-      let {handle} = job
-
-      const fileGet = await handle.getFile()
-
-      const arrayBuffer = await fileGet.arrayBuffer();
-      const hashBuffer = await crypto.subtle.digest('SHA-256', arrayBuffer);
-      const hashArray = Array.from(new Uint8Array(hashBuffer));
-      const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-
-      job.info = {
-        kind: handle.kind,
-
-        lastModified: fileGet.lastModified,
-        name: fileGet.name,
-        size: fileGet.size,
-        type: fileGet.type,
-
-        hash: {
-          sha256: hashHex
-        }
-      }
-    }
-  }
   
-  // alert("passing files")
-
-  for (let thread = 0; thread < threads; thread++) {
-    wait.push(threadPassFile(thread))
+  push(to){
+    for(let fromFile of this.overVeiw){
+      let toFile=to.getFromPath(fromFile.path)?.file
+      if(fromFile?.info?.size===toFile?.info?.size&&fromFile?.info?.hash?.sha256===toFile?.info?.hash?.sha256){
+          console.log('same')
+          continue
+      }
+      console.log('diff',fromFile,toFile)
+    }
   }
-  await Promise.all(wait)
-  return files
 }
 
-
-
-function passJson(inp,depth=0) {
-  if (typeof inp==='undefined') {return}
-  if (typeof inp==='function') {return 'func'}
-  if (typeof inp==='object') {
-    if(depth<0){
-      if(inp==null){return}
-      if(Object.keys(inp).length==0){return 'OOR'}
-      return inp
-    }
-    let ret={}
-    for (let a in inp){
-      ret[a]=passJson(inp[a],depth-1)
-    }
-    if(Object.keys(ret).length==0){return {}}
-    return ret
-  }
-  return inp
-}
+let fs = [new syncFs(0), new syncFs(1)]
 
 
 
