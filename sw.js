b4d5bbc5cee0108c4741b28d3fe0c3a468d2ac0d
Glitch (tankhellfire) <none>
2025-01-20 10:34:36 +0000

    👵🏇 Checkpoint ./sw.js:62919950/8863

--- sw.js
+++ sw.js
@@ -1,59 +1,4 @@
-class Db{
-  constructor(name,storeName){
-    this.name=name;
-    this.storeName=storeName;
-    return (async e=>{
-      await new Promise((res,rej)=>{
-        let req=indexedDB.open(name,1);
-
-        req.onupgradeneeded=e=>{
-          let db=e.target.result;
-          if(!db.objectStoreNames.contains(storeName))db.createObjectStore(storeName,{keyPath:"key"});
-        };
-
-        req.onsuccess=e=>{
-          this.dbInstance=e.target.result;
-          res(this);
-        };
-
-        req.onerror=e=>rej(`Database error:${e.target.errorCode}`)
-      });
-      return this
-    })();
-  }
-
-  set(key,data){return new Promise((res,rej)=>{
-    let req=this.dbInstance.transaction(this.storeName,"readwrite").objectStore(this.storeName).put({data,key});
-
-    req.onsuccess=e=>res(`written to "${key}"`)
-    req.onerror=e=>rej(`Write error:${e.target.errorCode}`)
-  })}
-
-  get(key){return new Promise((res,rej)=>{
-    let req=this.dbInstance.transaction(this.storeName,"readonly").objectStore(this.storeName).get(key)
-
-    req.onsuccess=e=>res(e.target.result?.data)
-    req.onerror=e=>rej(`Read error:${e.target.errorCode}`)
-  })}
-  
-  del(key){return new Promise((res,rej)=>{
-    let req=this.dbInstance.transaction(this.storeName,"readwrite").objectStore(this.storeName).delete(key);
-
-    req.onsuccess=e=>res(`deleted "${key}"`);
-    req.onerror=e=>rej(`Delete error: ${event.target.errorCode}`);
-  })}
-  
-
-  keys(){return new Promise((res,rej)=>{
-    let req=this.dbInstance.transaction(this.storeName,"readonly").objectStore(this.storeName).getAllKeys();
-
-    req.onsuccess=e=>res(e.target.result);
-    req.onerror=e=>rej(`Keys retrieval error:${e.target.errorCode}`);
-  })}
-
-}
-
-let db=new Db('tankhellfire','page-cache')
+const CACHE_NAME = 'page';
 
 let neverCache=[
   '/games/scratch/spacegamev5.html',
@@ -90,29 +35,36 @@ async function cache(a){
     console.log(`//never cache "${url}"`)
     return res.clone()
   }
-
-  db.then(async e=>{
-    await e.set(url.pathname,await res.clone().blob())
+  
+  caches.open(CACHE_NAME).then(cache=>{
+    cache.put(url.pathname,res.clone())
     console.log(`//updated cache "${url}"`)
   })
-  
-  let cachRes=(await db).get(url.pathname)
-  if(cachRes){
-    console.log(new Response(cachRes))
-  }
-  
+
   return res.clone()
 }
 
-self.addEventListener('fetch', (e) => {
-  const url=fixUrl(e.request.url)
+self.addEventListener('fetch', (event) => {
+  const url=fixUrl(event.request.url)
   
   if(url.hostname=='tankhellfire.glitch.me'){
-    if(url!=e.request.url){
-      console.log(`//redirecting "${e.request.url}"->"${url}"`)
-      return e.respondWith(Response.redirect(url, 301))
+    if(url!=event.request.url){
+      console.log(`//redirecting "${event.request.url}"->"${url}"`)
+      return event.respondWith(Response.redirect(url, 301))
     }
-    e.respondWith(cache(url))
+    event.respondWith(
+      caches.match(url).then((cachedResponse) => {
+        const a=cache(url)
+
+
+        if(cachedResponse){
+          console.log(`//returned cache "${url}"`)
+          return cachedResponse
+        }
+        console.log(`//returned fetch "${url}"`)
+        return a;
+      })
+    )
   }
   
 });
