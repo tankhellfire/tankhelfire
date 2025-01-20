67cc66c3b828bda4b13d70350a296dafe7d8871d
Glitch (tankhellfire) <none>
2025-01-20 11:45:41 +0000

    ⛺️💕 Checkpoint ./sw.js:62919950/5809 ./new.js:62919950/167

--- new.js
+++ new.js
@@ -1,1 +1,3 @@
 navigator.serviceWorker.register('/sw.js', { scope: '/' })
+let end=window.location.pathname.split('/').pop()
+if(end){if(!end.includes('.')){window.location.pathname+='index.html'}}else{window.location.pathname+='/index.html'}
--- sw.js
+++ sw.js
@@ -1,56 +1,92 @@
-let end=window.location.pathname.split('/').pop()
-if(end){if(end.includes('.')){window.location.pathname+='index.html'}}else{window.location.pathname+='/index.html'}
-// class Db{
-//   constructor(name,storeName){
-//     this.name=name;
-//     this.storeName=storeName;
-//     return (async e=>{
-//       await new Promise((res,rej)=>{
-//         let req=indexedDB.open(name,1);
+class Db{
+  constructor(name,storeName){
+    this.name=name;
+    this.storeName=storeName;
+    return (async e=>{
+      await new Promise((res,rej)=>{
+        let req=indexedDB.open(name,1);
 
-//         req.onupgradeneeded=e=>{
-//           let db=e.target.result;
-//           if(!db.objectStoreNames.contains(storeName))db.createObjectStore(storeName,{keyPath:"key"});
-//         };
+        req.onupgradeneeded=e=>{
+          let db=e.target.result;
+          if(!db.objectStoreNames.contains(storeName))db.createObjectStore(storeName,{keyPath:"key"});
+        };
 
-//         req.onsuccess=e=>{
-//           this.dbInstance=e.target.result;
-//           res(this);
-//         };
+        req.onsuccess=e=>{
+          this.dbInstance=e.target.result;
+          res(this);
+        };
 
-//         req.onerror=e=>rej(`Database error:${e.target.errorCode}`)
-//       });
-//       return this
-//     })();
-//   }
+        req.onerror=e=>rej(`Database error:${e.target.errorCode}`)
+      });
+      return this
+    })();
+  }
 
-//   set(key,data){return new Promise((res,rej)=>{
-//     let req=this.dbInstance.transaction(this.storeName,"readwrite").objectStore(this.storeName).put({data,key});
+  set(key,data){return new Promise((res,rej)=>{
+    let req=this.dbInstance.transaction(this.storeName,"readwrite").objectStore(this.storeName).put({data,key});
 
-//     req.onsuccess=e=>res(`written to "${key}"`)
-//     req.onerror=e=>rej(`Write error:${e.target.errorCode}`)
-//   })}
+    req.onsuccess=e=>res(`written to "${key}"`)
+    req.onerror=e=>rej(`Write error:${e.target.errorCode}`)
+  })}
 
-//   get(key){return new Promise((res,rej)=>{
-//     let req=this.dbInstance.transaction(this.storeName,"readonly").objectStore(this.storeName).get(key)
+  get(key){return new Promise((res,rej)=>{
+    let req=this.dbInstance.transaction(this.storeName,"readonly").objectStore(this.storeName).get(key)
 
-//     req.onsuccess=e=>res(e.target.result?.data)
-//     req.onerror=e=>rej(`Read error:${e.target.errorCode}`)
-//   })}
+    req.onsuccess=e=>res(e.target.result?.data)
+    req.onerror=e=>rej(`Read error:${e.target.errorCode}`)
+  })}
   
-//   del(key){return new Promise((res,rej)=>{
-//     let req=this.dbInstance.transaction(this.storeName,"readwrite").objectStore(this.storeName).delete(key);
+  del(key){return new Promise((res,rej)=>{
+    let req=this.dbInstance.transaction(this.storeName,"readwrite").objectStore(this.storeName).delete(key);
 
-//     req.onsuccess=e=>res(`deleted "${key}"`);
-//     req.onerror=e=>rej(`Delete error: ${event.target.errorCode}`);
-//   })}
+    req.onsuccess=e=>res(`deleted "${key}"`);
+    req.onerror=e=>rej(`Delete error: ${event.target.errorCode}`);
+  })}
   
 
-//   keys(){return new Promise((res,rej)=>{
-//     let req=this.dbInstance.transaction(this.storeName,"readonly").objectStore(this.storeName).getAllKeys();
+  keys(){return new Promise((res,rej)=>{
+    let req=this.dbInstance.transaction(this.storeName,"readonly").objectStore(this.storeName).getAllKeys();
 
-//     req.onsuccess=e=>res(e.target.result);
-//     req.onerror=e=>rej(`Keys retrieval error:${e.target.errorCode}`);
-//   })}
+    req.onsuccess=e=>res(e.target.result);
+    req.onerror=e=>rej(`Keys retrieval error:${e.target.errorCode}`);
+  })}
 
-// }
+}
+
+function fixUrl(url){
+  url=new URL(url,self.location.origin)
+  let end=url.pathname.split('/').pop()
+  if(end){
+    if(!end.includes('.')) url.pathname+='index.html'
+  }else window.location.pathname+='/index.html'
+  return url
+}
+function cache(url){
+  url
+}
+
+
+let db=new Db('tankhellfire','page-cache')
+
+let neverCache=[
+  '/games/scratch/spacegamev5.html',
+]
+
+let alwaysCache=[
+  '/txt',
+]
+
+
+self.addEventListener('install', (event) => {
+  console.log('//installing');
+  for(let a of alwaysCache){
+    console.log(`//always cache ${fixUrl(a)}`)
+    cache(a)
+  }
+  self.skipWaiting(); // Activate immediately
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('//activating');
+  event.waitUntil(clients.claim()); // Take control of all open pages
+});
