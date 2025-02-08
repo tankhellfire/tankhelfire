f835f2806ca437101b34c38762ad1a220ecdf8d1
Glitch (tankhellfire) <none>
2025-02-08 03:03:31 +0000

    👘🎳 Checkpoint ./audio/synth/audio.js:62919950/101 ./audio/synth/index.html:62919950/37

--- audio/synth/audio.js
+++ audio/synth/audio.js
@@ -12,13 +12,14 @@ class main extends AudioWorkletProcessor {
   }
 
   process(inputs, outputs) {
+    console.log("Worklet is processing!");
     const input = inputs[0];
     const output = outputs[0];
 
     if (input.length > 0) {
       for (let channel = 0; channel < input.length; channel++) {
         for (let i = 0; i < input[channel].length; i++) {
-          output[channel][i] = input[channel][i] * this.gain;
+          output[channel][i]=Math.random();
         }
       }
     }
