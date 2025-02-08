d03aace3841d4b966c8733ac088feb38014ccd0c
tankhellfire <tarek.lehman2009@gmail.com>
2025-02-08 10:35:02 +0800

    audio

---
+++ audio/synth/audio.js
@@ -0,0 +1,30 @@
+class main extends AudioWorkletProcessor {
+  constructor() {
+    super();
+    this.gain = 1.0;
+
+    this.port.onmessage = (event) => {
+      if (event.data.gain !== undefined) {
+        this.gain = event.data.gain;
+      }
+    }
+    ;
+  }
+
+  process(inputs, outputs) {
+    const input = inputs[0];
+    const output = outputs[0];
+
+    if (input.length > 0) {
+      for (let channel = 0; channel < input.length; channel++) {
+        for (let i = 0; i < input[channel].length; i++) {
+          output[channel][i] = input[channel][i] * this.gain;
+        }
+      }
+    }
+
+    return true;
+  }
+}
+
+registerProcessor('main', main);
