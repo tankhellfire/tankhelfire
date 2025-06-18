c85bb0d86282981de551c25a13a9ea8670849fad
tankh local <tankhellfire>
2025-06-18 19:38:40 +0800

    thanks glitch me

--- new.js
+++ new.js
@@ -1,4 +1,4 @@
-if(typeof __dirname=='undefined')__dirname='/'
+if(typeof __dirname=='undefined')__dirname='https://tankhellfire.github.io/tankhelfire/'
 ((a,b,c=a.pathname.split('/').pop())=>c.includes('.')||(a.pathname+=c?'/'+b:b))(window.location,'index.html')
 clog=console.log
 navigator.serviceWorker.register(`${__dirname}sw.js`,{scope:__dirname})
